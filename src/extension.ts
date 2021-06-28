3213ad7d921d1e9a9f69d6ecd009e963bc7d9e7d
William Cole <ioetbc@gmail.com>
2021-06-28 21:41:32 +0100

    adding in keybindings to hot reload the dev app

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,15 @@ import { HelloWorldPanel } from './HelloWorldPanel';
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('rubber-ducker.helloWorld', () => {
-		HelloWorldPanel.createOrShow(context.extensionUri)
+		HelloWorldPanel.createOrShow(context.extensionUri);
+	}));
+	context.subscriptions.push(vscode.commands.registerCommand('rubber-ducker.refresh', () => {
+		HelloWorldPanel.kill();
+		HelloWorldPanel.createOrShow(context.extensionUri);
+		// to start the app run the app in watch mode (fn f5 then you can press control r to reload the app becuase I have added a keybinding to the refresh comand)
+		setTimeout(() => {
+			vscode.commands.executeCommand("workbench.action.webview.openDeveloperTools");
+		}, 500);
 	}));
 }
 
--- webviews/components/HelloWorld.svelte
+++ webviews/components/HelloWorld.svelte
@@ -1,1 +1,1 @@
-<h1>Hello</h1>
+<h1>new old</h1>
