192f8a122b794bd40bf98b4dbb4397c934d58e09
William Cole <ioetbc@gmail.com>
2021-07-07 08:09:14 +0100

    using svelete to build basic ui

--- src/extension.ts
+++ src/extension.ts
@@ -12,14 +12,12 @@ export function activate(context: vscode.ExtensionContext) {
 		HelloWorldPanel.createOrShow(context.extensionUri);
 	}));
 	context.subscriptions.push(vscode.commands.registerCommand('rubber-ducker.refresh', async () => {
-		// HelloWorldPanel.kill();
-		// HelloWorldPanel.createOrShow(context.extensionUri);
-		// // to start the app run the app in watch mode (fn f5 then you can press control r to reload the app becuase I have added a keybinding to the refresh comand)
+		// // to start the app (control f5 then you can press control r to reload the app)
 		await vscode.commands.executeCommand("workbench.action.closeSidebar");
 		await vscode.commands.executeCommand("workbench.view.extension.rubber-ducker-sidebar-view");
-		setTimeout(() => {
-			vscode.commands.executeCommand("workbench.action.webview.openDeveloperTools");
-		}, 500);
+		// setTimeout(() => {
+		// 	vscode.commands.executeCommand("workbench.action.webview.openDeveloperTools");
+		// }, 500);
 	}));
 }
 
--- webviews/components/sidebar.svelte
+++ webviews/components/sidebar.svelte
@@ -1,11 +1,43 @@
-<script lang="ts"></script>
+<script lang="ts">
+  let todos: Array<{text: string, completed: boolean}> = [] 
+  let text = ''
+</script>
 
-<body>
-  <div>too good</div>
-</body>
+<form on:submit|preventDefault={() => {
+  todos = [{text: text, completed: false}, ...todos,]
+  text = ''
+}}>
+  <input bind:value={text} />
+</form>
+
+
+<ul>
+  {#each todos as todo (todo.text)}
+    <li on:click={() => todo.completed = !todo.completed} class:complete={todo.completed}>
+      {todo.text}
+    </li>
+  {/each}
+</ul>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <style>
   div {
-    color: blue;
+    color: red;
+  }
+  .complete {
+    text-decoration: line-through;
   }
 </style>
+
