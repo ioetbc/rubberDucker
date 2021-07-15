d0b0f78886d581adbddb8b48bedfdab99a7f8b16
William Cole <ioetbc@gmail.com>
2021-07-15 21:18:37 +0100

    create a postgres table

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,31 @@
-import express from 'express';
+import 'reflect-metadata'
+import express from 'express'
+import { createConnection } from 'typeorm'
+import { PROD } from './constants'
+import { join } from 'path'
+import { userInfo } from 'os'
+import { User } from './entities/User'
 
 const main = async () => {
+   await createConnection({
+      database: 'rubber-ducker',
+      type: 'postgres',
+      port: 5432,
+      logging: !PROD,
+      synchronize: !PROD,
+      entities: [join(__dirname, './entities/*.*')],
+      username: 'ioetbc',
+      password: 'therethere'
+   })
+
+   const user = await User.create({name: 'will'}).save()
+   console.log('new user ', user)
+
    const app = express();
    app.get('/', (_req, res) => {
       res.send('fuck yeah')
    });
+
    app.listen(3002, () => {
       console.log('lsitening on localhost 3002')
    });
