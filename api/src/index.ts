ef5203c75f9889733ab3646461f6f18a53ab2a86
William Cole <ioetbc@gmail.com>
2021-07-16 18:05:59 +0100

    login via github and update the db with username

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,13 @@
 import 'reflect-metadata'
+require('dotenv-safe').config()
 import express from 'express'
 import { createConnection } from 'typeorm'
 import { PROD } from './constants'
 import { join } from 'path'
-import { userInfo } from 'os'
 import { User } from './entities/User'
+import { Strategy as GitHubStrategy } from 'passport-github'
+import passport from 'passport'
+import jwt from 'jsonwebtoken'
 
 const main = async () => {
    await createConnection({
@@ -18,13 +21,34 @@ const main = async () => {
       password: 'therethere'
    })
 
-   const user = await User.create({name: 'will'}).save()
-   console.log('new user ', user)
-
    const app = express();
+
+   passport.serializeUser((user: any, done) => {
+      done(null, user.accessToken);
+    });
+   app.use(passport.initialize());
+   passport.use(new GitHubStrategy({
+      clientID: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      callbackURL: "http://localhost:3002/auth/github/callback"
+    }, async (_, __, profile, cb) => {
+       console.log('profile', profile)
+         let user = await User.findOne({ where: { githubId: profile.id! }})
+         if (!user) {
+            user = await User.create({ name: profile.username!, githubId: profile.id! }).save()
+         }
+         cb(null, { accessToken: jwt.sign({ userId: user.id }, process.env.JWT_SIGNATURE!, { expiresIn: '1y' }) })
+      }
+   ));
+   app.get('/auth/github', passport.authenticate('github', { session: false }))
+   app.get('/auth/github/callback', passport.authenticate('github', { session: false }),
+   (req: any, res) => {
+      res.redirect(`http://localhost:54321/auth/${req.user.accessToken}`)
+   })
+
    app.get('/', (_req, res) => {
       res.send('fuck yeah')
-   });
+   })
 
    app.listen(3002, () => {
       console.log('lsitening on localhost 3002')
