4b1238993a319ceefb60bde5aab51674efa9713c
William Cole <ioetbc@gmail.com>
2021-07-17 16:30:07 +0100

    get logged in user and displat info on the front end

--- api/src/index.ts
+++ api/src/index.ts
@@ -8,6 +8,7 @@ import { User } from './entities/User'
 import { Strategy as GitHubStrategy } from 'passport-github'
 import passport from 'passport'
 import jwt from 'jsonwebtoken'
+import cors from 'cors'
 
 const main = async () => {
    await createConnection({
@@ -22,7 +23,7 @@ const main = async () => {
    })
 
    const app = express();
-
+   app.use(cors({ origin: 'vscode-webview://webviewview-rubber-ducker-sidebar' }))
    passport.serializeUser((user: any, done) => {
       done(null, user.accessToken);
     });
@@ -50,6 +51,36 @@ const main = async () => {
    app.get('/auth/github/callback', passport.authenticate('github', { session: false }),
    (req: any, res) => {
       res.redirect(`http://localhost:54321/auth/${req.user.accessToken}`)
+   })
+
+   // returns the current user logged in based of the JWT token passed from the extension
+   app.get('/me', async (req, res) => {
+      const authHeader = req.headers.authorization
+      if (!authHeader) {
+         res.send({ user: null })
+         return
+      }
+
+      const token = authHeader.split(' ')[1]
+      if (!token) {
+         res.send({ user: null })
+         return
+      }
+
+      let userId
+      try {
+         const payload: any = jwt.verify(token, process.env.JWT_SIGNATURE!)
+         userId = payload.userId
+      } catch (error) {
+         res.send({ user: null })
+         return
+      }
+      if (!userId) {
+         res.send({ user: null })
+         return
+      }
+      const user = await User.findOne(userId)
+      res.send({ user })
    })
 
    app.get('/', (_req, res) => {
--- extension/webviews/components/sidebar.svelte
+++ extension/webviews/components/sidebar.svelte
@@ -1,22 +1,44 @@
 <script lang="ts">
   import { onMount } from 'svelte';
+  let accessToken = ''
   let todos: Array<{text: string, completed: boolean}> = [] 
   let text = ''
-  onMount(() => {
-    window.addEventListener('message', event => {
-        const message = event.data; // The json data that the extension sent
-        console.log({message})
+  let loading = true
+  let user: { name: string; id: number } | null = null
+  onMount(async () => {
+    window.addEventListener('message', async event => {
+        const message = event.data;
         switch (message.type) {
             case 'new-todo':
-          todos = [
-            { text: message.value, completed: false },
-            ...todos
-          ]
+              todos = [
+                { text: message.value, completed: false },
+                ...todos
+              ]
+            case 'token':
+              // fetches current user using the accessToken
+              accessToken = message.value
+              const response = await fetch(`${apiBaseUrl}/me`, {
+                headers: { authorization: `Bearer ${accessToken}`}
+              })
+              const data = await response.json()
+              user = data.user
+              loading = false
             break;
         }
     });
+
+    // post message to SidebarProvider with value of get-token
+    tsvscode.postMessage({ type: 'get-token' })
   })
 </script>
+
+{#if loading}
+  <div>loading...</div>
+{:else if user}
+  <pre>{JSON.stringify(user, null, 2)}</pre>
+{:else}
+  <div>no user is logged in</div>
+{/if}
 
 <form on:submit|preventDefault={() => {
   todos = [{text: text, completed: false}, ...todos,]
